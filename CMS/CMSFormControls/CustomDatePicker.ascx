ad62454d5ff50be6043082f5dc98341ca1f50d55
Saimurali Sriramaneni <Saimurali.Sriramaneni@k2america.com>
2023-08-17 16:47:35 +0530

    Bootstrap DatePicker

---
+++ CMS/CMSFormControls/CustomDatePicker.ascx
@@ -0,0 +1,29 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="CustomDatePicker.ascx.cs" Inherits="CMSApp.CMSFormControls.CustomDatePicker" %>
+
+
+<link rel="stylesheet" type="text/css" href="/CMSModules/Content/CMSDesk/App_Themes/DatePicker/jquery.datetimepicker.css"/>
+<script src="/CMSModules/Content/CMSDesk/App_Themes/DatePicker/jquery.js"></script>
+<script src="/CMSModules/Content/CMSDesk/App_Themes/DatePicker/jquery.datetimepicker.full.js"></script>   
+               
+<asp:TextBox ID="datePicker" runat="server"  CssClass="form-control EditingFormCalendarTextBox CalendarTextBox input-width-58 hasDatepicker" autocomplete="off"></asp:TextBox>           
+
+        
+
+            <script>
+                /*jslint browser:true*/
+                /*global jQuery, document*/
+
+                jQuery(document).ready(function () {
+                    'use strict';
+
+                    jQuery('#m_c_f_BootstrapDatepicker_datePicker').datetimepicker();
+                });
+            </script>
+   
+
+
+
+
+
+
+
---
+++ CMS/CMSFormControls/CustomDatePicker.ascx.cs
@@ -0,0 +1,79 @@
+﻿using CMS.Base;
+using CMS.FormEngine.Web.UI;
+using CMS.Helpers;
+using CMS.Localization;
+using System;
+using System.Web.UI.DataVisualization.Charting;
+
+namespace CMSApp.CMSFormControls
+{
+    public partial class CustomDatePicker : FormEngineUserControl
+    {
+        
+        public override object Value
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(datePicker.Text))
+                {
+                    string dateTime = datePicker.Text;                    
+                    return DateTime.Parse(dateTime);
+                }
+                
+                return datePicker.Text;                
+            }                
+            set
+            {
+                if (value != null)
+                {
+                   DateTime dateTime = DateTime.Parse(value.ToString());
+                   datePicker.Text = dateTime.ToString("yyyy/MM/dd HH:mm");                    
+                }
+            }
+        }
+           
+        public override bool IsValid()
+        { 
+            if (string.IsNullOrEmpty(datePicker.Text.ToString()))
+            {
+                // Sets the form control validation error message
+                this.ValidationError = "Please select a Date.";
+                return false;               
+            }
+            else
+            {                
+                return true;
+            }
+        }
+        public int SelectorWidth
+        {
+            get
+            {
+                return ValidationHelper.GetInteger(GetValue("SelectorWidth"), 0);
+            }
+            set
+            {
+                SetValue("SelectorWidth", value);
+            }
+        }
+
+        protected void EnsureItems()
+        {
+            // Applies the width specified through the parameter of the form control if it is valid
+            if (SelectorWidth > 0)
+            {
+                datePicker.Width = SelectorWidth;
+            }
+        }
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) 
+            {
+                EnsureItems();
+            }
+        }
+
+    }
+}
